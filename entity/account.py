19d4984c1bc0c8154029e77d77f6b12dcd393e80
Caio-GBrayner <caiogomesbrayner@gmail.com>
2025-02-14 00:33:17 -0300

    Getters and setters for entities, deposit and withdrawal methods, holder added to account

--- entity/account.py
+++ entity/account.py
@@ -1,10 +1,37 @@
+from entity import Client
+
 class Account:
-    balance: float
-    account_id: int
 
-    def __init__(self, balance: float, account_id: int):
-        self.balance = balance
-        self.account_id = account_id
+    def __init__(self, balance: float, account_id: int, holder: Client):
+        self.__balance = balance
+        self.__account_id = account_id
+        self.__holder = holder
 
-    def details(self):
-        return f"Balance: {self.balance}, Account ID: {self.account_id}"
+    @property
+    def balance(self):
+        return self.__balance
+
+    @property
+    def holder(self):
+        return self.__holder
+
+    @holder.setter
+    def holder(self, holder: Client):
+        self.__holder = holder
+
+    @property
+    def account_id(self):
+        return self.__account_id
+
+    def deposit(self, amount: float):
+        self.__balance += amount
+
+    def withdrawal(self, amount: float):
+        if self.__balance >= amount:
+            self.__balance -= amount
+            print("Successful withdrawal!")
+        else:
+            raise ValueError("Invalid operation, insufficient balance!")
+
+    def details_to_string(self):
+        return f"Balance: {self.__balance}, Account ID: {self.__account_id}, Holder: {self.__holder}"
--- entity/client.py
+++ entity/client.py
@@ -1,8 +1,48 @@
 class Client:
-    def __init__(self, name, age, phone):
-        self.name = name
-        self.age = age
-        self.phone = phone
+    name: str
+    age: int
+    phone: str
+
+    def __init__(self, name: str, age: int, phone: str):
+        self.__name = name
+        self.__age = age
+        self.__phone = phone
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, name):
+        self.__name = name
+
+    @property
+    def age(self):
+        return self.__age
+
+    @age.setter
+    def age(self, age):
+        if age < 0:
+            raise ValueError("The age cannot be less than 0")
+        else:
+            self.__age = age
+
+    @property
+    def phone(self):
+        return self.__phone
+
+    @phone.setter
+    def phone(self, phone):
+        if not phone.isdigit():
+            raise ValueError("The telephone number must contain only digits")
+        if len(phone) != 10:
+            raise ValueError("The telephone number must contain only ten digits")
+        else:
+            self.__phone = self.format_phone(phone)
+
+    @staticmethod
+    def format_phone(phone: str) -> str:
+        return f"({phone[:2]}){phone[2:6]}-{phone[6:]}"
 
     def details(self):
-        return f"Name: {self.name}, Age: {self.age}, Phone: {self.phone}"
+        return f"Name: {self.__name}, Age: {self.__age}, Phone: {self.__phone}"
